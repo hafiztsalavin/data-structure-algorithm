850d3ec363437ba37ad3b9fa928d3bc9336e5045
tslvn <hafiztsalavin@gmail.com>
2022-07-08 10:48:57 +0700

    Time: 65 ms (19.89%), Space: 6.8 MB (63.73%) - LeetHub

--- 704-binary-search/704-binary-search.go
+++ 704-binary-search/704-binary-search.go
@@ -1,31 +1,7 @@
 func search(nums []int, target int) int {
     size := len(nums)   
-    if size == 1 {
-        return singleArr(nums, target)
-    } else if size == 2 {
-        return doubleArr(nums, target)
-    } else {
-        
-        return binarySearch(nums, 0, size-1, target)
-    }
-}
-
-func singleArr(nums []int, target int) int {
-    if nums[0] != target{
-        return -1
-    } else {
-        return 0
-    }
-    
-}
-
-func doubleArr(nums []int, target int) int {
-    for i := 0; i < len(nums); i++ {
-        if nums[i] == target {
-            return i
-        }
-    }
-    return -1
+   
+    return binarySearch(nums, 0, size-1, target)
 }
 
 func binarySearch(nums []int, start, end, target int) int {
