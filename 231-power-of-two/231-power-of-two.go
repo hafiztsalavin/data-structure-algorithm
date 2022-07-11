c1b8cabdbfb6087de9a6c80c9f2c3be179fa1dae
tslvn <hafiztsalavin@gmail.com>
2022-07-11 10:04:29 +0700

    Time: 0 ms (100.00%), Space: 2 MB (78.96%) - LeetHub

--- 231-power-of-two/231-power-of-two.go
+++ 231-power-of-two/231-power-of-two.go
@@ -1,14 +1,20 @@
 func isPowerOfTwo(n int) bool {
-    if n < 1{
-        return false
+    if n == 1{
+        return true
     }
     
-    for n > 1{
-        if n%2 == 1{
+    if n % 2 != 0 || n == 0 || n < 1{
+        return false
+    } 
+    
+    
+    nums := n
+    for nums > 1 {
+        if nums % 2 == 1 {
             return false
         }
         
-        n /=2
+        nums /= 2
     }
     
     return true
