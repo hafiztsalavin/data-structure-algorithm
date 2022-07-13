b090ba943468c85d37e89ee8fa9deb10c0371351
tslvn <hafiztsalavin@gmail.com>
2022-07-13 13:44:52 +0700

    Time: 44 ms (61.86%), Space: 7.3 MB (84.54%) - LeetHub

---
+++ 599-minimum-index-sum-of-two-lists/599-minimum-index-sum-of-two-lists.go
@@ -0,0 +1,28 @@
+func findRestaurant(list1 []string, list2 []string) []string {
+    result := []string{}
+    max := len(list1) + len(list2)
+    
+    mapRestaurant := map[string]int{}
+    for i, restaurant := range list1 {
+        mapRestaurant[restaurant] = i+1 
+    }
+
+    for i, restaurant2 := range list2{
+        if value, restaurant := mapRestaurant[restaurant2]; restaurant{
+            tempMax := (i+1)+value
+            
+            
+            fmt.Println(restaurant2, tempMax)    
+
+            if tempMax < max {
+                max = tempMax
+                result = result[:0]
+                result = append(result, restaurant2)
+            } else if tempMax == max{
+                result = append(result, restaurant2)
+            }
+        }
+    }
+    return result    
+}
+    
