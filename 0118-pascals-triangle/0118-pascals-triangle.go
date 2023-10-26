1913fee548ceaa9bb3cf3ddd996a015108588633
tslvn <hafiztsalavin@gmail.com>
2023-10-27 06:56:23 +0700

    Time: 1 ms (78.95%), Space: 2.4 MB (6.07%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.go
+++ 0118-pascals-triangle/0118-pascals-triangle.go
@@ -1,21 +1,14 @@
 func generate(numRows int) [][]int {
-    ans := [][]int{{1}}
+    triangle := make([][]int, numRows)
 
-    for i := 1; i < numRows; i++{
-        prevArr := ans[i-1] 
-
-        newArr := make([]int,i+1)
-        newArr[0], newArr[i] = 1,1
-        if i > 1 {
-            for j:= 0; j < i-1; j++{
-                num := prevArr[j] + prevArr[j+1]
-                newArr[j+1] = num
-            }
-
-        }   
-        ans = append(ans, newArr)
+    for i := 0; i < numRows; i++{
+        triangle[i] = make([]int, i+1)
+        triangle[i][0] = 1
+        triangle[i][i] = 1
+        for j := 1; j < i; j++{
+            triangle[i][j] = triangle[i-1][j-1] + triangle[i-1][j]
+        }
     }
-
-    return ans
     
+    return triangle
 }
