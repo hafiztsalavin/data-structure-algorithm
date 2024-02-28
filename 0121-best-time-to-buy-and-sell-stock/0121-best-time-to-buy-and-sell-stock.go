0960c980e251e1ff192b2a69b076db1e7ee353ae
tslvn <hafiztsalavin@gmail.com>
2024-02-28 14:55:08 +0700

    Time: 129 ms (21.47%), Space: 10.2 MB (7.48%) - LeetHub

---
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.go
@@ -0,0 +1,13 @@
+func maxProfit(prices []int) int {
+
+    maxSell := 0
+    min := prices[0]
+    for _, price := range prices{
+        if price < min{
+            min = price
+        } else if price - min > maxSell {
+            maxSell = price - min
+        }
+    }
+    return maxSell
+}
